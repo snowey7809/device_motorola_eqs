3e165977f69485de8417f67a094967e0d54418c2
snow <154572788+snowey7809@users.noreply.github.com>
2024-02-23 20:11:49 +0530

    eqs: switch to forked display repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 echo Cloning device specific HALs
 rm -rf hardware/qcom-caf/sm8450/display
-git clone https://github.com/Project-Elixir/hardware_qcom-caf_sm8450_display hardware/qcom-caf/sm8450/display
+git clone https://github.com/snowey7809/hardware_qcom-caf_sm8450_display.git -b dx hardware/qcom-caf/sm8450/display
 git clone https://github.com/snowey7809/android_device_motorola_sm8475-common.git -b 14.0 device/motorola/sm8475-common
 git clone https://github.com/nishant6342/vendor_motorola_sm8475-common -b UNO vendor/motorola/sm8475-common
 git clone https://gitea.com/nishant6342/vendor_motorola_eqs -b UNO vendor/motorola/eqs
