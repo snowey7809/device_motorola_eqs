bc48a148dd3744fa2d8e222b80b0584bf73d2dd9
snow <154572788+snowey7809@users.noreply.github.com>
2024-02-23 16:10:45 +0530

    eqs: switch to lineage kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,5 +6,5 @@ git clone https://github.com/nishant6342/vendor_motorola_sm8475-common -b UNO ve
 git clone https://gitea.com/nishant6342/vendor_motorola_eqs -b UNO vendor/motorola/eqs
 git clone --depth=1 https://github.com/nishant6342/device_motorola_eqs-kernel device/motorola/eqs-kernel
 git clone --depth=1 https://github.com/nishant6342/hardware_motorola -b UNO hardware/motorola
-git clone --depth=1 https://github.com/nishant6342/android_kernel_motorola_sm8475 -b workbranch kernel/motorola/sm8475
+git clone --depth=1 https://github.com/nishant6342/android_kernel_motorola_sm8475 -b lineage-20 kernel/motorola/sm8475
 echo eqs: end cloning device specific repos
